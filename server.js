18d63f4c25957483a7081fdb58400d815e8bcc36
Saige Losli <saige.losli@gmail.com>
2018-06-28 20:53:27 -0700

    Add project page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get( '/about', (req, res) => {
   } );
 });
 
+app.get( '/projects', (req, res) => {
+  res.render( 'projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 // /bad - send back json with errorMessage property
 app.get( '/bad', (req, res) => {
   res.send({
